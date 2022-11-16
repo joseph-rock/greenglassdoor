b9c59bc7471ef10a923c9b03b28e26ae220e505f
Joseph <josephrock225@gmail.com>
2022-11-16 15:53:55 -0800

    Init Project

---
+++ greenglassdoor.ts
@@ -0,0 +1,11 @@
+
+function isGood(word: string): boolean {
+    for(let i = 1; i < word.length; i++) {
+        if(word[i] === word[i-1]) {
+            return true
+        }
+    }
+    return false
+}
+
+console.log(isGood("wood"))
