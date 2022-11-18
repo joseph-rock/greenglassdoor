7d954b0313d1c4c9ed512d0532ee2202da1381a1
Joseph <josephrock225@gmail.com>
2022-11-17 19:17:36 -0800

    Restructured package

--- api.ts
+++ api.ts
@@ -1,33 +1,27 @@
-import { config } from "https://deno.land/x/dotenv/mod.ts";
-import { isGood } from "./greenglassdoor.ts";
+import { config } from "./deps.ts";
+import { isGood } from "./green_glass_door.ts";
 
 async function synonymList(word: string): Promise<string[]> {
   const env = config();
   const url = `${env.API_URL}${word}?key=${env.API_KEY}`;
-  const strength = 4;
 
   const resp = await fetch(url);
   const data = await resp.json();
-  const syns = data[0].meta.syns;
 
-  return syns.length > strength ? syns.slice(0, strength).flat() : syns.flat();
+  return data.reduce(
+    (synonyms: string[], list: any) => synonyms.concat(list.meta.syns.flat()),
+    [],
+  );
 }
 
 export async function goodSynonym(word: string): Promise<string> {
   const synList = await synonymList(word);
   const list = synList.filter((word) => isGood(word));
-  console.log(list);
   return list[Math.floor(Math.random() * list.length)];
 }
 
 export async function badSynonym(word: string): Promise<string> {
   const synList = await synonymList(word);
   const list = synList.filter((word) => !isGood(word));
-  console.log(list);
   return list[Math.floor(Math.random() * list.length)];
 }
-
-const word = "ring";
-const good = await goodSynonym(word);
-const bad = await badSynonym(word);
-console.log(good, bad);
