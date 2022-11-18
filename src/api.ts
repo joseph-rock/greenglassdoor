182d2874c35d54146fb31aab16749d3e495c7d41
Joseph <josephrock225@gmail.com>
2022-11-17 18:38:38 -0800

    Work in progress. Added functions to fetch single good or bad synonym. Need to add error handling and defaults if sufficient word is not found

--- src/api.ts
+++ src/api.ts
@@ -1,20 +1,33 @@
 import { config } from "https://deno.land/x/dotenv/mod.ts";
+import { isGood } from "./greenglassdoor.ts";
 
 async function synonymList(word: string): Promise<string[]> {
   const env = config();
   const url = `${env.API_URL}${word}?key=${env.API_KEY}`;
-  const strength = 3;
+  const strength = 4;
 
   const resp = await fetch(url);
   const data = await resp.json();
   const syns = data[0].meta.syns;
 
-  return syns.reduce(
-    (total: string[], list: string[], i: number) =>
-      i < strength && list[i] ? total.concat(list) : total,
-    [],
-  );
+  return syns.length > strength ? syns.slice(0, strength).flat() : syns.flat();
 }
 
-const result = await synonymList("parrot");
-console.log(result);
+export async function goodSynonym(word: string): Promise<string> {
+  const synList = await synonymList(word);
+  const list = synList.filter((word) => isGood(word));
+  console.log(list);
+  return list[Math.floor(Math.random() * list.length)];
+}
+
+export async function badSynonym(word: string): Promise<string> {
+  const synList = await synonymList(word);
+  const list = synList.filter((word) => !isGood(word));
+  console.log(list);
+  return list[Math.floor(Math.random() * list.length)];
+}
+
+const word = "ring";
+const good = await goodSynonym(word);
+const bad = await badSynonym(word);
+console.log(good, bad);
