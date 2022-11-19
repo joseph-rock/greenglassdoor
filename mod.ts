7264714f6f8fcf8b180f44d67d29f06da178fefd
Joseph <josephrock225@gmail.com>
2022-11-18 22:16:12 -0800

    Rename and refactor

--- mod.ts
+++ mod.ts
@@ -1,8 +1,8 @@
-import { badSynonym, goodSynonym, isGood } from "./utils.ts";
+import { badSynonym, goodSynonym, passesDoor } from "./utils.ts";
 
 async function main() {
   const word = Deno.args[0];
-  if (isGood(word)) {
+  if (passesDoor(word)) {
     const syn = await badSynonym(word);
     console.log(`${word} is good, ${syn} is bad.`);
   } else {
--- utils.ts
+++ utils.ts
@@ -5,17 +5,13 @@ interface Synonyms {
   bad: string[];
 }
 
-function randomElement(list: string[]): string {
-  return list[Math.floor(Math.random() * list.length)];
-}
-
 async function syns(word: string): Promise<Synonyms> {
   const gSyns: string[] = [];
   const bSyns: string[] = [];
 
   await synonymList(word)
     .then((list) =>
-      list.map((word) => isGood(word) ? gSyns.push(word) : bSyns.push(word))
+      list.map((word) => passesDoor(word) ? gSyns.push(word) : bSyns.push(word))
     );
 
   gSyns.length === 0 ? gSyns.push("good") : gSyns;
@@ -25,15 +21,6 @@ async function syns(word: string): Promise<Synonyms> {
     good: gSyns,
     bad: bSyns,
   };
-}
-
-export function isGood(word: string): boolean {
-  for (let i = 1; i < word.length; i++) {
-    if (word[i] === word[i - 1]) {
-      return true;
-    }
-  }
-  return false;
 }
 
 export async function goodSynonym(word: string): Promise<string> {
@@ -46,4 +33,17 @@ export async function badSynonym(word: string): Promise<string> {
   return await syns(word)
     .then((value) => value.bad)
     .then((list) => randomElement(list));
+}
+
+export function passesDoor(word: string): boolean {
+  for (let i = 1; i < word.length; i++) {
+    if (word[i] === word[i - 1]) {
+      return true;
+    }
+  }
+  return false;
+}
+
+function randomElement(list: string[]): string {
+  return list[Math.floor(Math.random() * list.length)];
 }
