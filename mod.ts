dd76843bc1b7651fde2b0b049d05197a572e3741
Joseph <josephrock225@gmail.com>
2022-11-28 18:19:39 -0800

    Reorganization

--- mod.ts
+++ mod.ts
@@ -1,4 +1,59 @@
-import { badSynonym, goodSynonym, passesDoor } from "./utils.ts";
+import { lookupWord } from "./thesaurus_api.ts";
+
+interface Synonyms {
+  good: string[];
+  bad: string[];
+}
+
+async function syns(word: string): Promise<Synonyms> {
+  const gSyns: string[] = [];
+  const bSyns: string[] = [];
+
+  await lookupWord(word)
+    .then((value) =>
+      value.reduce(
+        (total: string[], synList: any) =>
+          total.concat(synList.meta.syns.flat()),
+        [],
+      )
+    )
+    .then((list) =>
+      list.map((word) => passesDoor(word) ? gSyns.push(word) : bSyns.push(word))
+    );
+
+  gSyns.length === 0 ? gSyns.push("good") : gSyns;
+  bSyns.length === 0 ? bSyns.push("bad") : bSyns;
+
+  return {
+    good: gSyns,
+    bad: bSyns,
+  };
+}
+
+async function goodSynonym(word: string): Promise<string> {
+  return await syns(word)
+    .then((value) => value.good)
+    .then((list) => randomElement(list));
+}
+
+async function badSynonym(word: string): Promise<string> {
+  return await syns(word)
+    .then((value) => value.bad)
+    .then((list) => randomElement(list));
+}
+
+function randomElement(list: string[]): string {
+  return list[Math.floor(Math.random() * list.length)];
+}
+
+function passesDoor(word: string): boolean {
+  for (let i = 1; i < word.length; i++) {
+    if (word[i] === word[i - 1]) {
+      return true;
+    }
+  }
+  return false;
+}
 
 async function main() {
   const word = Deno.args[0];
--- thesaurus_api.ts
+++ thesaurus_api.ts
@@ -1,21 +1,10 @@
 import { config } from "./deps.ts";
 
-export async function synonymList(word: string): Promise<string[]> {
+export async function lookupWord(word: string): Promise<string[]> {
   const env = config();
   const url = `${env.API_URL}${word}?key=${env.API_KEY}`;
 
   return await fetch(url)
     .then((value) => value.json())
-    .then((value) =>
-      value.reduce(
-        (total: string[], synList: any) =>
-          total.concat(synList.meta.syns.flat()),
-        [],
-      )
-    )
-    .catch(() => fallback());
-}
-
-function fallback(): string[] {
-  return ["good", "bad"];
+    .catch(() => []);
 }
